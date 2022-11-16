dba3caac5a65352679bf6873a8fcbf26ce9ddc66
Артём Одийко <odiykoartem@gmail.com>
2022-11-16 11:57:05 +0200

    Added new page

---
+++ src/pages/Suppliers.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import { Info } from "../components/Info";
+
+const Suppliers: FC = () => {
+	return <Info title="Suppliers" />;
+};
+
+export default Suppliers;
