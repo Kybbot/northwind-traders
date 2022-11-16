b5584a9ce45c96c69129ea0654027bdb87584f8b
Артём Одийко <odiykoartem@gmail.com>
2022-11-16 11:56:06 +0200

    Added new route

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 
 import Home from "./pages/Home";
+import Suppliers from "./pages/Suppliers";
 
 const App: FC = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
 				<Route index element={<Home />} />
+				<Route path="suppliers" element={<Suppliers />} />
 			</Route>
 		</Routes>
 	);
