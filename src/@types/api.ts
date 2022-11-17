0821f5dee41b9b68cc32764f70821aee7900983a
Артём Одийко <odiykoartem@gmail.com>
2022-11-17 13:50:19 +0200

    Added types

---
+++ src/@types/api.ts
@@ -0,0 +1,65 @@
+export type Supplier = {
+	id: number;
+	SupplierID: string;
+	CompanyName: string;
+	ContactName: string;
+	ContactTitle: string;
+	Address: string;
+	City: string;
+	Region: string;
+	PostalCode: string;
+	Country: string;
+	Phone: string;
+	Fax: string;
+	HomePage: string;
+};
+
+export type Prosuct = {
+	id: number;
+	ProductID: string;
+	ProductName: string;
+	SupplierID: string;
+	CategoryID: string;
+	QuantityPerUnit: string;
+	UnitPrice: string;
+	UnitsInStock: string;
+	UnitsOnOrder: string;
+	ReorderLevel: string;
+	Discontinued: string;
+};
+
+export type Order = {
+	TotalProductsDiscount: number;
+	TotalProductsPrice: number;
+	TotalProductsItems: number;
+	TotalProducts: number;
+	OrderId: string;
+	CustomerID: string;
+	EmployeeID: string;
+	OrderDate: string;
+	RequiredDate: string;
+	ShippedDate: string;
+	ShipVia: string;
+	Freight: string;
+	ShipName: string;
+	ShipAddress: string;
+	ShipCity: string;
+	ShipRegion: string;
+	ShipPostalCode: string;
+	ShipCountry: string;
+	ProductId: string;
+};
+
+export type ApiResult<T> = {
+	data: T;
+	success: true;
+};
+
+export type ApiError = {
+	error: {
+		message: string;
+	};
+	success: false;
+};
+
+export type ApiResponse<T> = ApiError | ApiResult<T>;
